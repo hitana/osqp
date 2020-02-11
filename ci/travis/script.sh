641dcb5ea989eb751f0c5a0fb5e1b99b909da191
hitana.cpp <hitana.cpp@gmail.com>
2020-02-10 23:46:50 -0500

    [-] Travis: change script

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -4,33 +4,34 @@ set -e
 # Add MKL shared libraries to the path
 # This unfortunately does not work in travis OSX if I put the export command
 # in the install.sh (it works on linux though)
-export MKL_SHARED_LIB_DIR=`ls -rd ${CONDA_ROOT}/pkgs/*/ | grep mkl-2 | head -n 1`lib:`ls -rd ${CONDA_ROOT}/pkgs/*/ | grep intel-openmp- | head -n 1`lib
+#export MKL_SHARED_LIB_DIR=`ls -rd ${CONDA_ROOT}/pkgs/*/ | grep mkl-2 | head -n 1`lib:`ls -rd ${CONDA_ROOT}/pkgs/*/ | grep intel-openmp- | head -n 1`lib
 
-echo "MKL shared library path: ${MKL_SHARED_LIB_DIR}"
+#echo "MKL shared library path: ${MKL_SHARED_LIB_DIR}"
 
-if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
-    export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${MKL_SHARED_LIB_DIR}
-else if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${MKL_SHARED_LIB_DIR}
-fi
-fi
+#if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
+#    export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${MKL_SHARED_LIB_DIR}
+#else if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+#    export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${MKL_SHARED_LIB_DIR}
+#fi
+#fi
 
 
-# ---------------------------------------------------
-# Test D interface
-# Compile and test OSQP
-echo "Change directory to Travis build ${TRAVIS_BUILD_DIR}"
-echo "Testing OSQP with standard configuration"
+echo "Testing D interface with arguments-> arch=${ARCH}, DFLOAT=${DFLOAT}, DLONG=${DLONG}, USE_EMBEDDED=${USE_EMBEDDED} EMBEDDED=${EMBEDDED}"
 cd ${TRAVIS_BUILD_DIR}
-mkdir build
-# cd build
-# cmake -G "Unix Makefiles" -DCOVERAGE=ON -DUNITTESTS=ON ..
-# make
-meson -D COVERAGE=true -D UNITTESTS=true -D NDEBUG=true build
+rm -rf build
+meson --arch "$ARCH" -D NDEBUG=true -DUNITTESTS=${UNITTESTS} -DPRINTING=${PRINTING} -DPROFILING=${PROFILING} -DCTRLC=${CTRLC} -DDFLOAT=${DFLOAT} -DDLONG=${DLONG} -DCOVERAGE=${COVERAGE} -DUSE_EMBEDDED=${USE_EMBEDDED} -DEMBEDDED=${EMBEDDED} build
 ninja -C build -j8
 
-${TRAVIS_BUILD_DIR}/build/osqp_tester
+if [[ "${USE_EMBEDDED}" == false ]]; then
+    echo "Running OSQP demo"
+    ${TRAVIS_BUILD_DIR}/build/osqp_demo
+    echo "Running OSQP tester"
+    ${TRAVIS_BUILD_DIR}/build/osqp_tester
+fi
+
+
 # Perform code coverage (only in Linux case)
+# ---------------------------------------------------
 #if [[ $TRAVIS_OS_NAME == "linux" ]]; then
 #    cd ${TRAVIS_BUILD_DIR}/build
 #    lcov --directory . --capture -o coverage.info # capture coverage info
@@ -43,87 +44,24 @@ ${TRAVIS_BUILD_DIR}/build/osqp_tester
 #    coveralls-lcov coverage.info # uploads to coveralls
 #fi
 
-if [[ $TRAVIS_OS_NAME == "linux" ]]; then
-    echo "Testing OSQP with valgrind (disabling MKL pardiso for memory allocation issues)"
-    cd ${TRAVIS_BUILD_DIR}
-    rm -rf build
-    # mkdir build    
-    # cd build
-    #disable PARDISO since intel instructions in MKL
-    #cause valgrind 3.11 to fail
-    # cmake -G "Unix Makefiles" -DENABLE_MKL_PARDISO=OFF -DUNITTESTS=ON ..
-    # make
-
-    meson -D ENABLE_MKL_PARDISO=false -D UNITTESTS=true build
-    ninja -C build -j8
-
-    valgrind --suppressions=${TRAVIS_BUILD_DIR}/.valgrind-suppress.supp --leak-check=full --gen-suppressions=all --track-origins=yes --error-exitcode=42 ${TRAVIS_BUILD_DIR}/build/osqp_tester
-fi
-
-
-
+# Valgrind
 # ---------------------------------------------------
-echo "Running OSQP demo"
-cd ${TRAVIS_BUILD_DIR}
-rm -rf build
-meson -D DLONG=false -D UNITTESTS=true -D NDEBUG=true build
-ninja -C build -j8
-${TRAVIS_BUILD_DIR}/build/osqp_demo
-
-# ---------------------------------------------------
-
-echo "Testing OSQP with floats"
-cd ${TRAVIS_BUILD_DIR}
-rm -rf build
-meson -D DFLOAT=true -D UNITTESTS=true -D NDEBUG=true build
-ninja -C build -j8
-${TRAVIS_BUILD_DIR}/build/osqp_tester
-
-echo "Testing OSQP with doubles"
-cd ${TRAVIS_BUILD_DIR}
-rm -rf build
-meson -D DFLOAT=false -D UNITTESTS=true -D NDEBUG=true build
-ninja -C build -j8
-${TRAVIS_BUILD_DIR}/build/osqp_tester
-
-echo "Testing OSQP with long integers"
-cd ${TRAVIS_BUILD_DIR}
-rm -rf build
-meson -D DLONG=true -D UNITTESTS=true -D NDEBUG=true build
-ninja -C build -j8
-${TRAVIS_BUILD_DIR}/build/osqp_tester
-
-echo "Testing OSQP without long integers"
-cd ${TRAVIS_BUILD_DIR}
-rm -rf build
-meson -D DLONG=false -D UNITTESTS=true -D NDEBUG=true build
-ninja -C build -j8
-${TRAVIS_BUILD_DIR}/build/osqp_tester
-
-echo "Building OSQP with embedded=1"
-cd ${TRAVIS_BUILD_DIR}
-rm -rf build
-meson -D USE_EMBEDDED=true -D EMBEDDED='1' -D NDEBUG=true build
-ninja -C build -j8
-
-echo "Building OSQP with embedded=2"
-cd ${TRAVIS_BUILD_DIR}
-rm -rf build
-meson -D USE_EMBEDDED=true -D EMBEDDED='2' -D NDEBUG=true build
-ninja -C build -j8
-
-
-echo "Testing OSQP without printing"
-cd ${TRAVIS_BUILD_DIR}
-rm -rf build
-meson -D PRINTING=false -D UNITTESTS=true -D NDEBUG=true build
-ninja -C build -j8
-${TRAVIS_BUILD_DIR}/build/osqp_tester
+#if [[ $TRAVIS_OS_NAME == "linux" ]]; then
+#    echo "Testing OSQP with valgrind (disabling MKL pardiso for memory allocation issues)"
+#    cd ${TRAVIS_BUILD_DIR}
+#    rm -rf build
+#    #disable PARDISO since intel instructions in MKL cause valgrind 3.11 to fail
+#    # cmake -G "Unix Makefiles" -DENABLE_MKL_PARDISO=OFF -DUNITTESTS=ON ..
+#    # make
+#
+#    meson -D ENABLE_MKL_PARDISO=false -D UNITTESTS=true build
+#    ninja -C build -j8
+#    valgrind --suppressions=${TRAVIS_BUILD_DIR}/.valgrind-suppress.supp --leak-check=full --gen-suppressions=all --track-origins=yes --error-exitcode=42 ${TRAVIS_BUILD_DIR}/build/osqp_tester
+#fi
 
 
 # Test custom memory management
 # ---------------------------------------------------
-
 #echo "Test OSQP custom allocators"
 #cd ${TRAVIS_BUILD_DIR}
 #rm -rf build
